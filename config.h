5e5c571183ce94bf75cf4261df2d620bfa7c327d
rdbo <rdbodev@gmail.com>
2022-10-14 13:35:22 -0300

    added background image patch

--- config.h
+++ config.h
@@ -10,3 +10,6 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* Background image path, should be available to the user above */
+static const char* background_image = "/usr/share/backgrounds/wallpaper-blur.png";
