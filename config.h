c28275d2ecd92787bc053951ac82ba52145d657f
rdbo <rdbodev@gmail.com>
2022-10-14 13:31:17 -0300

    added config.h

---
+++ config.h
@@ -0,0 +1,12 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#101a20",     /* after initialization */
+	[INPUT] =  "#00ffc8",     /* during input */
+	[FAILED] = "#ff0000",     /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
